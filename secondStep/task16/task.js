c66532a21c4e85902e2c613a1b2250b0ba4ac09e
ArthurFree <lc940401@gmail.com>
2016-06-26 18:53:14 +0800

    提交任务16、17

--- secondStep/task16/task.js
+++ secondStep/task16/task.js
@@ -7,20 +7,33 @@
  * };
  */
 var aqiData = {};
-
+var dataCityName = document.getElementById("aqi-city-input");
+var dataCityValue = document.getElementById("aqi-value-input");
 /**
  * 从用户输入中获取数据，向aqiData中增加一条数据
  * 然后渲染aqi-list列表，增加新增的数据
  */
 function addAqiData() {
 
+	var cityName = dataCityName.value.trim();
+	var cityValue = dataCityValue.value.trim();
+
+	if(!cityName.match(/^[A-Za-z\u4E00-\u9FA5]+$/)) return console.log("请输入汉字或字母");
+	if(!cityValue.match(/^\d+$/)) return console.log("请输入整数数字");
+
+	aqiData[cityName] = cityValue;
 }
 
 /**
  * 渲染aqi-table表格
  */
 function renderAqiList() {
-
+	var table = document.getElementById("aqi-table");
+	var item = "<tr><th>城市名称</th><th>空气质量指数</th><th>操作</th></tr>";
+	for(var key in aqiData) {
+		item += "<tr><td>" + key + "</td><td>" + aqiData[key] + "</td><td><button data-city='" + key + "'>删除</button></tr>"
+	}
+	table.innerHTML = key ? item : "";
 }
 
 /**
@@ -28,18 +41,18 @@ function renderAqiList() {
  * 获取用户输入，更新数据，并进行页面呈现的更新
  */
 function addBtnHandle() {
-  addAqiData();
-  renderAqiList();
+  	addAqiData();
+  	renderAqiList();
 }
 
 /**
  * 点击各个删除按钮的时候的处理逻辑
  * 获取哪个城市数据被删，删除数据，更新表格显示
  */
-function delBtnHandle() {
+function delBtnHandle(city) {
   // do sth.
-
-  renderAqiList();
+  	delete aqiData[city]
+  	renderAqiList();
 }
 
 function init() {
@@ -48,6 +61,14 @@ function init() {
 
   // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
 
+  var btn = document.getElementById("add-btn");
+  btn.onclick = addBtnHandle;
+
+  var delBtn = document.getElementById("aqi-table");
+  delBtn.addEventListener('click', function(event) {
+  	if(event.target.nodeName.toLowerCase() === "button") 
+  		delBtnHandle.call(null, event.target.dataset.city);
+  });
 }
 
 init();
