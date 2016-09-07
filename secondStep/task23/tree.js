536bd07e53cfa4d179d0e883fc61a9d4b2bbd4f9
ArthurFree <lc940401@gmail.com>
2016-09-07 16:25:31 +0800

    完成任务23，增加搜索功能

--- secondStep/task23/tree.js
+++ secondStep/task23/tree.js
@@ -15,6 +15,8 @@
 })();
 function TreeWalker() {
 	this.stack = [];
+	this.isWalking = false;
+	this.isSearching = false;
 }
 
 TreeWalker.prototype.preOrder = function(node) {
@@ -40,8 +42,9 @@ TreeWalker.prototype.postOrder = function(node) {
 }
 
 TreeWalker.prototype.search = function(node) {
+	this.isSearching = true;
 	this.preOrder(node);
-	console.log(this.stack);
+	this.animation();
 }
 
 TreeWalker.prototype.animation = function() {
@@ -49,23 +52,80 @@ TreeWalker.prototype.animation = function() {
 	var iter = 0;
 	var stack = this.stack;
 	var self = this;
+	var keyword = document.querySelector("input[name=search]").value.trim().toLocaleLowerCase();
 	var speeder = document.querySelector("#speeder");
 
 	self.stack = [];
+	self.searchArr = [];
 
 	if (!self.isWalking) {
 		self.isWalking = true;
-		stack[iter].style.backgroundColor = "#F125C2";
+		stack[iter].style.backgroundColor = "rgb(50, 180, 250)";
 		timer = setInterval(function(){
-			if (iter == stack.length-1) {
-				stack[iter].style.backgroundColor = "#FFFFFF";
-				self.isWalking = false;
-				clearInterval(timer);
+			if (self.isSearching) {
+
+				if (iter == stack.length-1) {
+
+					if (stack[iter].firstChild.nodeValue.trim().toLocaleLowerCase() == keyword) {
+						stack[iter].style.backgroundColor = "red";
+					} else {
+						stack[iter].style.backgroundColor = "#FFFFFF";
+					}
+
+					self.isWalking = false;
+					self.isSearching = false;
+					clearInterval(timer);
+
+				} else {
+
+					++iter;
+
+					if (stack[iter-1].firstChild.nodeValue.trim().toLocaleLowerCase() !== keyword) {
+						stack[iter-1].style.backgroundColor = "#FFFFFF";
+					}
+
+					if (stack[iter].firstChild.nodeValue.trim().toLocaleLowerCase() == keyword) {
+						stack[iter].style.backgroundColor = "red";
+					} else {
+						stack[iter].style.backgroundColor = "rgb(50, 180, 250)";
+					}
+
+				}
+
 			} else {
-				++iter;
-				stack[iter-1].style.backgroundColor = "#FFFFFF";
-				stack[iter].style.backgroundColor = "#F125C2";
+
+				if (iter == stack.length-1) {
+					stack[iter].style.backgroundColor = "#FFFFFF";
+					self.isWalking = false;
+					self.isSearching = false;
+					clearInterval(timer);
+				} else {
+					++iter;
+					stack[iter-1].style.backgroundColor = "#FFFFFF";
+					stack[iter].style.backgroundColor = "rgb(50, 180, 250)";
+				}
+
 			}
 		}, speeder.value);
+
 	}
+}
+
+/* 事件绑定 */
+function addHandler(element, type, handler) {
+	if (element.addEventListener) {
+		addHandler = function(element, type, handler) {
+			element.addEventListener(type, handler, false);
+		}
+	} else if (element.attachEvent) {
+		addHandler = function(element, type, handler) {
+			element.attachEvent("on"+type, handler);
+		}
+	} else {
+		addHandler = function(element, type, handler) {
+			element["on" + type] = handler;
+		}
+	}
+
+	return addHandler(element, type, handler);
 }
